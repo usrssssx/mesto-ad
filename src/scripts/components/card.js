60c338f192456e6976752aa9c46c7abaa494afda
usrssssx <swiftkey965@gmail.com>
2026-05-22 20:14:08 +0300

    Исправленный проект

--- src/scripts/components/card.js
+++ src/scripts/components/card.js
@@ -15,23 +15,42 @@ const getTemplate = () => {
 
 export const createCardElement = (
   data,
-  { onPreviewPicture, onLikeIcon, onDeleteCard }
+  { onPreviewPicture, onLikeIcon, onDeleteCard, onInfoClick, userId }
 ) => {
   const cardElement = getTemplate();
   const likeButton = cardElement.querySelector(".card__like-button");
+  const likeCount = cardElement.querySelector(".card__like-count");
+  const infoButton = cardElement.querySelector(".card__control-button_type_info");
   const deleteButton = cardElement.querySelector(".card__control-button_type_delete");
   const cardImage = cardElement.querySelector(".card__image");
+  const cardOwnerId = data.owner ? data.owner._id : null;
+  const cardId = data._id;
+  const isLikedByUser = data.likes
+    ? data.likes.some((like) => like._id === userId)
+    : false;
 
   cardImage.src = data.link;
   cardImage.alt = data.name;
   cardElement.querySelector(".card__title").textContent = data.name;
 
   if (onLikeIcon) {
-    likeButton.addEventListener("click", () => onLikeIcon(likeButton));
+    if (likeCount) {
+      likeCount.textContent = data.likes ? data.likes.length : 0;
+    }
+    if (isLikedByUser) {
+      likeButton.classList.add("card__like-button_is-active");
+    }
+    likeButton.addEventListener("click", () => onLikeIcon(likeButton, cardId, likeCount));
   }
 
-  if (onDeleteCard) {
-    deleteButton.addEventListener("click", () => onDeleteCard(cardElement));
+  if (cardOwnerId && userId && cardOwnerId !== userId) {
+    deleteButton.remove();
+  } else if (onDeleteCard) {
+    deleteButton.addEventListener("click", () => onDeleteCard(cardElement, cardId));
+  }
+
+  if (infoButton && onInfoClick) {
+    infoButton.addEventListener("click", () => onInfoClick(cardId));
   }
 
   if (onPreviewPicture) {
